d795af1a30c2167b36076d46abf65f2af6373bf4
whalewalker <mongodbms@gmail.com>
2024-02-29 16:29:15 +0100

    Added swagger doc config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  setupSwagger(app);
+
   await app.listen(3000);
 }
+
 bootstrap();
+
+const setupSwagger = (app: INestApplication): void => {
+  const config = new DocumentBuilder()
+    .setTitle('Book Shelf')
+    .setDescription('Simple CRUD operation for Book API')
+    .setVersion('1.0')
+    .addTag('books')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+};
